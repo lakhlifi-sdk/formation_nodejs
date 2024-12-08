2dddb665a8bf0d33cc598af6902ed060e555ed41
lakhlifi-sdk <57691109+lakhlifi-sdk@users.noreply.github.com>
2024-12-08 14:56:22 +0100

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     url: 'mongodb://localhost:27017/todo-list',
-    uri : 'mongodb+srv://lakhlifi:root@cluster0.ftqvsza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    uri : 'mongodb+srv://lakhlifi:.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 }
